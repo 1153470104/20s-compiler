7d8024bd5559998b59985ed175bdebc22688bf09
1153470104 <t34_shen@163.com>
2020-04-25 21:47:18 +0800

    V2.1: try to add a GUI

--- src/CompilerGUI.java
+++ src/CompilerGUI.java
@@ -4,13 +4,14 @@ public class CompilerGUI {
     private JButton translate;
     private JPanel p1;
     private JTextField t1;
-    private JTextField t2;
+    private JTextArea textArea1;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("CompilerGUI");
-        frame.setContentPane(new CompilerGUI().p1);
+        JFrame frame = new JFrame("Compiler");          //new JFrame
+        frame.setSize(400, 200);
+        frame.setContentPane(new CompilerGUI().p1);             //set content pane
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible(true);                                 //set visible
     }
 }
